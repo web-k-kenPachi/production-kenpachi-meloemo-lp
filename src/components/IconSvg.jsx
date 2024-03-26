aa7ec25502423c93843ff40e853c4e049e50f4b2
kk-macmini <web.k.kawamura@gmail.com>
2024-03-26 11:34:59 +0900

    調整

--- src/components/IconSvg.jsx
+++ src/components/IconSvg.jsx
@@ -15,7 +15,7 @@ export const Heart = (props) => {
     <svg
       width="100%"
       height="100%"
-      viewBox="-20 0 300 280"
+      viewBox="-10 -40 300 280"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
